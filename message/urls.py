c90bc2a7400498d345841474649cb51ba92d9b4e
goutarouh <gouta.soccer.0220@outlook.jp>
2019-08-13 19:11:26 +0900

    chat

--- message/urls.py
+++ message/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'message'
 urlpatterns = [
+    path('', views.index, name = 'index'),
     path('index/', views.index, name = 'index'),
     path('<str:room_name>/', views.room, name='room'),
 ]
